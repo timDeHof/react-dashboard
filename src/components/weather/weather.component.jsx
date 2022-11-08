b87f073fdef31239fd0cd22b2041db44a5622f76
timDeHof <this.is.tim.dehof@gmail.com>
2022-11-08 17:19:49 -0500

    refactored component

--- src/components/weather/weather.component.jsx
+++ src/components/weather/weather.component.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import "./weather.styles.css";
+import React, { useState, useEffect } from "react"
+import "./weather.styles.css"
 const weatherData = {
   iconUrl: "",
   cityName: "",
@@ -7,34 +7,38 @@ const weatherData = {
   feels_like: 0,
   temp_max: 0,
   temp_min: 0,
-};
+}
+const api_key = "1f2f2021d1330570f678948fe7371d12"
 const Weather = () => {
-  const [weather, setWeather] = useState(weatherData);
+  const [weather, setWeather] = useState(weatherData)
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      fetch(
-        `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`
-      )
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("Weather data not available");
-          }
-          return res.json();
-        })
-        .then((data) =>
-          setWeather({
-            iconUrl: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
-            cityName: data.name,
-            temp: data.main.temp,
-            feels_like: data.main.feels_like,
-            temp_max: data.main.temp_max,
-            temp_min: data.main.temp_min,
+    const getWeatherData = () => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial&appid=${api_key}`
+        )
+          .then((res) => {
+            if (!res.ok) {
+              throw Error("Weather data not available")
+            }
+            return res.json()
           })
-        );
-    });
-  }, []);
-  const { iconUrl, cityName, temp, feels_like, temp_max, temp_min } = weather;
+          .then((data) =>
+            setWeather({
+              iconUrl: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+              cityName: data.name,
+              temp: data.main.temp,
+              feels_like: data.main.feels_like,
+              temp_max: data.main.temp_max,
+              temp_min: data.main.temp_min,
+            })
+          )
+      })
+    }
+    getWeatherData()
+  }, [])
+  const { iconUrl, cityName, temp, feels_like, temp_max, temp_min } = weather
   return (
     <div className='weather-container'>
       <div className='weather-container-top'>
@@ -54,12 +58,10 @@ const Weather = () => {
         <p className='weather-temp'>{Math.round(temp)}º</p>
       </div>
       <div className='weather-container-bottom'>
-        <p className='weather-feels_like-temp'>
-          feels like {Math.round(feels_like)}º
-        </p>
+        <p className='weather-feels_like-temp'>feels like {Math.round(feels_like)}º</p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Weather;
+export default Weather
